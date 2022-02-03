3bd7dccf7f82b17ef295857905037bc8a3cbeb81
DargyLobo <dargijuli@gmail.com>
2022-02-02 19:21:09 -0500

    se sube el proyecto con el tracker 3 terminado

---
+++ programate/Febrero/proyectos/Covid-19/proyecto3_Covid19Track3/js/tracket4.js
@@ -0,0 +1,10 @@
+// Add active class to the current button (highlight it)
+let icono = document.getElementById("nav");
+let btns = icono.getElementsByClassName("navbar");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  let current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
