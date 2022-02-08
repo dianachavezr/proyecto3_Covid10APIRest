e79759a82d58985be369410d07da8a0fdbc553a7
DargyLobo <dargijuli@gmail.com>
2022-02-07 23:16:00 -0500

    tracker3.

--- programate/Febrero/proyectos/Covid-19/proyecto3_Covid19Track3/js/tracket4.js
+++ programate/Febrero/proyectos/Covid-19/proyecto3_Covid19Track3/js/tracket4.js
@@ -1,41 +1,119 @@
-// Add active class to the current button (highlight it)
-let icono = document.getElementById("nav");
-let btns = icono.getElementsByClassName("navbar");
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  let current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
-
-
-let url= "https://disease.sh/v3/covid-19/all"
-fetch(url)
+const columnas1 = document.querySelector('#columnas1')
+fetch('https://disease.sh/v3/covid-19/countries?sort=cases')
 .then(response => response.json())
-.then(data => mostrarData(data))
-.catch(error => console.log(error))
 
-const mostrarData = (data) => {
-    console.log(data)
-    //TEMPLATE STRING PARA TRAER LOS DATOS
-    let body = ''
+.then(data => {
+const limit = 10;
+let datas = data.slice(0, limit)
+datas.map(dat => {
+columnas1.innerHTML +=
 
-    for(let i = 0; i<data.length; i++){
-        body += `<tr>
-                <td><img src = ${data[i].countryInfo.flag} + style = 'height:15px;width:30px'></td>
-                <td>${data[i].country}</td>
-                <td>${data[i].cases}</td>
-                <td>${data[i].todayCases}</td>
-                <td>${data[i].deaths}</td>
-                <td>${data[i].todayDeaths}</td>
-                <td>${data[i].recovered}</td>
-                <td>${data[i].active}</td>
-                <td>${data[i].critical}</td>
-                <td>${data[i].tests}</td>     
-                </tr>`
+ `<div>
+    <img src="${dat.countryInfo.flag}" + style="width: 26px; height: 26px">
+    <h6>${dat.country}</h6>  
+    <p>${dat.cases}</p>
+  </div>
+`
 
-    }   
-    
-    document.getElementById('data').innerHTML = body
-}
+});
+});
+
+const columnas2 = document.querySelector('#columnas2')
+fetch('https://disease.sh/v3/covid-19/countries?sort=todayCases')
+.then(response => response.json())
+
+.then(data => {
+const limit = 10;
+let datas = data.slice(0, limit)
+datas.map(dat => {
+columnas2.innerHTML +=
+
+ `<div>
+    <img src="${dat.countryInfo.flag}" + style="width: 26px; height: 26px">
+    <h6>${dat.country}</h6>  
+    <p>${dat.cases}</p>
+  </div>
+`
+
+});
+}); 
+
+const columnas3 = document.querySelector('#columnas3')
+fetch('https://disease.sh/v3/covid-19/countries?sort=deaths')
+.then(response => response.json())
+
+.then(data => {
+const limit = 10;
+let datas = data.slice(0, limit)
+datas.map(dat => {
+columnas3.innerHTML +=
+
+ `<div>
+    <img src="${dat.countryInfo.flag}" + style="width: 26px; height: 26px">
+    <h6>${dat.country}</h6>  
+    <p>${dat.cases}</p>
+  </div>
+`
+
+});
+}); 
+
+const columnas4 = document.querySelector('#columnas4')
+fetch('https://disease.sh/v3/covid-19/countries?sort=todayDeaths')
+.then(response => response.json())
+
+.then(data => {
+const limit = 10;
+let datas = data.slice(0, limit)
+datas.map(dat => {
+columnas4.innerHTML +=
+
+ `<div>
+    <img src="${dat.countryInfo.flag}" + style="width: 26px; height: 26px">
+    <h6>${dat.country}</h6>  
+    <p>${dat.cases}</p>
+  </div>
+`
+
+});
+}); 
+
+const columnas5 = document.querySelector('#columnas5')
+fetch('https://disease.sh/v3/covid-19/countries?sort=active')
+.then(response => response.json())
+
+.then(data => {
+const limit = 10;
+let datas = data.slice(0, limit)
+datas.map(dat => {
+columnas5.innerHTML +=
+
+ `<div>
+    <img src="${dat.countryInfo.flag}" + style="width: 26px; height: 26px">
+    <h6>${dat.country}</h6>  
+    <p>${dat.cases}</p>
+  </div>
+`
+
+});
+}); 
+
+const columnas6 = document.querySelector('#columnas6')
+fetch('https://disease.sh/v3/covid-19/countries?sort=recovered')
+.then(response => response.json())
+
+.then(data => {
+const limit = 10;
+let datas = data.slice(0, limit)
+datas.map(dat => {
+columnas6.innerHTML +=
+
+ `<div>
+    <img src="${dat.countryInfo.flag}" + style="width: 26px; height: 26px">
+    <h6>${dat.country}</h6>  
+    <p>${dat.cases}</p>
+  </div>
+`
+
+});
+}); 
